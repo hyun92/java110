f720a232789fcd89c050540450211a42f8adcd2d
hyun <rudgns1523@naver.com>
2018-09-14 10:29:27 +0900

    DAO 클래스 정리

--- java110-project/src/main/java/bitcamp/java110/cms/dao/StudentDao.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/StudentDao.java
@@ -6,12 +6,12 @@ import bitcamp.java110.cms.domain.Student;
 
 public interface StudentDao {
     int insert(Student student) 
-            throws MandatoryValueDaoException, DuplicationDaoException;
+           ;
     List<Student> findAll();
     Student findByEmail(String email);
     default Student findByNo(int no) {return null;}
-    default int delete(String email) {return 0;}
-    default int delete(int no) {return 0;}
+    int delete(int no);  
+    //default 는 기존의 것 유지보수 하거나  프로그램에 혼란을 일으키지 않으면서 새로운 규칙 추가시 필요한거다.
 }
 
 
